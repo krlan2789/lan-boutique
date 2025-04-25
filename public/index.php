2e9aec4951a8a38f1d997f77d79436dfaaffb441
krlan2789 <ahlipdkno.2@gmail.com>
2025-04-25 17:55:26 +0700

    Modify public/index.php for production mode, and move project files after deployment . .

--- public/index.php
+++ public/index.php
@@ -4,14 +4,36 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+// // Determine if the application is in maintenance mode...
+// if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+//     require $maintenance;
+// }
+
+// // Register the Composer autoloader...
+// require __DIR__.'/../vendor/autoload.php';
+
+// // Bootstrap Laravel and handle the request...
+// (require_once __DIR__.'/../bootstrap/app.php')
+//     ->handleRequest(Request::capture());
+
+
+
+
+
+// Adjust paths based on the environment
+$basePath = __DIR__ . '/../';
+if (app()->environment('production')) {
+    $basePath .= 'build/';
+}
+
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = $basePath . 'storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+require $basePath . 'vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
+(require_once $basePath . 'bootstrap/app.php')
     ->handleRequest(Request::capture());
